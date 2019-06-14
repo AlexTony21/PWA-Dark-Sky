1683534410b7bed50bb41b33c9ddab8d0ea0eaa1
AlexTony21 <alexandretony.huynh@hotmail.fr>
2019-06-14 16:56:20 +0200

    Essai Fusion

--- public/service-worker.js
+++ public/service-worker.js
@@ -47,6 +47,30 @@ const FILES_TO_CACHE = [
   '/images/wind.svg',
 ];
 
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'Jvoubez';
+  const options = {
+    body: event.data.text(),
+    icon: 'images/icon.png',
+    badge: 'images/badge.png'
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
+
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow('http://www.dustloop.com/wiki/index.php?title=BlazBlue_Cross_Tag_Battle')
+  );
+});
+
 self.addEventListener('install', (evt) => {
   console.log('[ServiceWorker] Install');
   // CODELAB: Precache static resources here.
